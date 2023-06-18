bd0ea411060b0cdaeb8de75c901300527c4de2ff
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-06-18 15:47:34 -0300

    feat[Generic CSV]: Updated CSVHandler & Formatter

--- people-job/src/csv/CSVHandler.java
+++ people-job/src/csv/CSVHandler.java
@@ -1,6 +1,5 @@
 package csv;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
@@ -13,21 +12,45 @@ import model.Candidato;
 
 public class CSVHandler {
     private static final Path CANDIDATO_PATH = Paths.get("./cadastro.csv");
+    static {
+        try {
+            if (!Files.exists(CANDIDATO_PATH)) {
+                Files.createFile(CANDIDATO_PATH);
+                writeLine(CANDIDATO_PATH, "nome;cpf;dataNascimento;sexo;logradouro;numero;bairro;complemento;cidade;estado;email;telefone;celular;celularWhats;profissao;empresa;salario;empregoAtual;pretencaoMinima;pretencaoMaxima;habilidades;");
+            }
+        } catch (Exception e) {
+            System.err.println(String.format(
+                "people-job.csv.CSVHandler | Initialization Error: Falha ao criar arquivo necessário \"{%s}\"", 
+                CANDIDATO_PATH
+            ));
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeLine (Path path, String line) {
+        try (FileChannel channel = FileChannel.open(
+            path,
+            StandardOpenOption.CREATE,
+            StandardOpenOption.APPEND
+        )) {
+            channel.position(channel.size()).write(ByteBuffer.wrap(line.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            System.err.println(String.format(
+                "people-job.csv.CSVHandler.writeLine | Error writing line. Path: \"%s\". line: \"%s\"",
+                path,
+                line
+            ));
+            e.printStackTrace();
+        }
+    }
 
     public static void writeCandidato (Candidato candidato) {
         try {
-            if (!Files.exists(CANDIDATO_PATH)) Files.createFile(CANDIDATO_PATH);
-            FileChannel channel = FileChannel.open(
+            writeLine(
                 CANDIDATO_PATH,
-                StandardOpenOption.CREATE,
-                StandardOpenOption.APPEND
+                Formatter.<Candidato>getFormatted(candidato)
             );
-            channel.position(channel.size()).write(ByteBuffer.wrap(
-                Formatter.getFormatted(candidato).getBytes(StandardCharsets.UTF_8)
-            ));
-            channel.close();
-            System.out.println("Line written successfully.");
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- people-job/src/csv/Formatter.java
+++ people-job/src/csv/Formatter.java
@@ -1,35 +1,47 @@
 package csv;
 
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
 
 import model.Candidato;
 
 public class Formatter {
     private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd");
 
-    public static String getFormatted(Candidato candidato) {
-        return new StringBuilder(candidato.getNome())
-        .append(candidato.getCpf()).append(";")
-        .append(candidato.getDataNascimento().format(dateFormat)).append(";")
-        .append(candidato.getSexo()).append(";")
-        .append(candidato.getLogradouro()).append(";")
-        .append(candidato.getNumero()).append(";")
-        .append(candidato.getBairro()).append(";")
-        .append(candidato.getComplemento()).append(";")
-        .append(candidato.getCidade()).append(";")
-        .append(candidato.getBairro()).append(";")
-        .append(candidato.getEmail()).append(";")
-        .append(candidato.getTelefone()).append(";")
-        .append(candidato.getCelular()).append(";")
-        .append(candidato.isCelularWhats()).append(";")
-        .append(candidato.getProfissao()).append(";")
-        .append(candidato.getEmpresa()).append(";")
-        .append(candidato.getSalario()).append(";")
-        .append(candidato.hasEmpregoAtual()).append(";")
-        .append(candidato.getPretencaoMinima()).append(";")
-        .append(candidato.getPretencaoMaxima()).append(";")
-        .append(candidato.getHabilidades()).append(";")
-        .append("\n")
-        .toString();
+    private static final HashMap<Class<?>, List<Function<?, ?>>> TYPE_TO_SUPPLIER_HMAP = new HashMap<>(); 
+    static {
+        TYPE_TO_SUPPLIER_HMAP.put(Candidato.class, List.of(
+            candidato -> ((Candidato)candidato).getNome(),
+            candidato -> ((Candidato)candidato).getCpf(),
+            candidato -> ((Candidato)candidato).getDataNascimento().format(dateFormat),
+            candidato -> ((Candidato)candidato).getSexo(),
+            candidato -> ((Candidato)candidato).getLogradouro(),
+            candidato -> ((Candidato)candidato).getNumero(),
+            candidato -> ((Candidato)candidato).getBairro(),
+            candidato -> ((Candidato)candidato).getComplemento(),
+            candidato -> ((Candidato)candidato).getCidade(),
+            candidato -> ((Candidato)candidato).getBairro(),
+            candidato -> ((Candidato)candidato).getEmail(),
+            candidato -> ((Candidato)candidato).getTelefone(),
+            candidato -> ((Candidato)candidato).getCelular(),
+            candidato -> ((Candidato)candidato).isCelularWhats(),
+            candidato -> ((Candidato)candidato).getProfissao(),
+            candidato -> ((Candidato)candidato).getEmpresa(),
+            candidato -> ((Candidato)candidato).getSalario(),
+            candidato -> ((Candidato)candidato).hasEmpregoAtual(),
+            candidato -> ((Candidato)candidato).getPretencaoMinima(),
+            candidato -> ((Candidato)candidato).getPretencaoMaxima(),
+            candidato -> ((Candidato)candidato).getHabilidades()
+        ));
+    }
+
+    @SuppressWarnings("unchecked") public static <T> String getFormatted (T item) throws Exception {
+        StringBuilder sb = new StringBuilder(System.lineSeparator());
+        for (Function<T, ?> function : (List<Function<T, ?>>) (List<?>) TYPE_TO_SUPPLIER_HMAP.get(item.getClass())) {
+            sb.append(function.apply(item)).append(";");
+        }
+        return sb.toString();
     }
 }
